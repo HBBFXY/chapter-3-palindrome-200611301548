088ab6a4af866259b3a0f68614cb878ce0c18ae2
200611301548 <3446790582@qq.com>
2025-09-19 11:11:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,2 @@
-def is_palindrome_five_digit():
-    # 获取用户输入
-    num_input = input("请输入一个5位数字：")
-    
-    # 验证输入长度和是否为纯数字
-    if len(num_input) != 5 or not num_input.isdigit():
-        print("错误提示")
-        return
-    
-    # 判断回文数
-    if num_input == num_input[::-1]:
-        print("是回文数")
-    else:
-        print("不是回文数")
-
-# 执行函数
-is_palindrome_five_digit()
+n = input()
+print("错误提示" if len(n)!=5 or not n.isdigit() else "是回文数" if n==n[::-1] else "不是回文数")
