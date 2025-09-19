b32c97d78014ed2b7b268208415b4170215ad1c3
200611301548 <3446790582@qq.com>
2025-09-19 11:30:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ num = input().strip()
 if len(num) != 5 or not num.isdigit():
     print("错误提示")
 else:
-    print("是回文数" if num == num[::-1] else "不是回文数")
+    print("是回文数" if num == num[::-1] else "不是回文数"）
